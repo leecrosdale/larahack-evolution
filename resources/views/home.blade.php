79969cfd09e14e15087694ad393e5927eef446bf
Lee Crosdale <accounts@leecrosdale.com>
2019-02-27 21:17:35 +0000

    Added meta refresh for now

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('head')
+    <meta http-equiv="refresh" content="5">
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
